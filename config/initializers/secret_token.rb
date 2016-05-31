8588b310020a8ff63ee606aba4e8894973183cc3
mohan <mohan.singh@infibeam.net>
2016-05-31 19:24:38 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpiynAssignment::Application.config.secret_token = 'f30833b97ee4db41dd900e7a90d786029718646184a1ab7c1c9dc61c4bbe28aa5c55c61f743a351e3dd155351f6aedd1627948d035308fe89f8114e74f7f56ac'
